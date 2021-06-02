a9f4002b9be60de57d963d80953266299c88f22f
baltaza288 <666fisi2018666@gmail.com>
2021-06-02 08:17:58 +0200

    02.06.2021

---
+++ AdventOfCode_create_files_function.py
@@ -0,0 +1,19 @@
+
+###################################################################
+### Function to create all files for one year of Advent_of_Code ###
+###################################################################
+
+import os
+year = input("witch year is it?\n")
+for x in range(1,26,1):
+    num = x
+    day = "{:02d}".format(num)
+    path = 'c:/Temp/temporary/AdventOfCode_' + year + '/' + day + '.Dec_' + year
+    challenge = os.path.join(path, day + '.Dec_' + year + '_Challenge.txt')
+    input = os.path.join(path, day + '.Dec_' + year + '_Input.txt')
+    solution = os.path.join(path, day + '.Dec_' + year + '_solution.py')
+    if not os.path.exists(path):
+        os.makedirs(path)
+    f = open(challenge, "a")
+    f = open(input, "a")
+    f = open(solution, "a")
