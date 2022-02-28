5075085f7624062886887f5617602d89d9272f38
Baltaza288 <666fisi2018666@gmail.com>
2022-02-28 11:12:33 +0100

    Create Files and Folders

--- AdventOfCode_create_files_function.py
+++ AdventOfCode_create_files_function.py
@@ -10,10 +10,12 @@ for x in range(1,26,1):
     day = "{:02d}".format(num)
     path = 'c:/Temp/temporary/AdventOfCode_' + year + '/' + day + '.Dec_' + year
     challenge = os.path.join(path, day + '.Dec_' + year + '_Challenge.txt')
+    challenge_d = os.path.join(path, day + '.Dec_' + year + '_Challenge_D.txt')
     input = os.path.join(path, day + '.Dec_' + year + '_Input.txt')
     solution = os.path.join(path, day + '.Dec_' + year + '_solution.py')
     if not os.path.exists(path):
         os.makedirs(path)
-    f = open(challenge, "a")
-    f = open(input, "a")
-    f = open(solution, "a")
+#    f = open(challenge, "a")
+    f = open(challenge_d, "a")
+#    f = open(input, "a")
+#    f = open(solution, "a")
